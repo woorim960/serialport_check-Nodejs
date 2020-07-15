cd7a3098a02879934428aefd88039dc5dcb2b451
박우림 <woorimprog@gmail.com>
2020-07-15 16:14:36 +0900

    serialport-check.js 주석 처리

--- serialport-check.js
+++ serialport-check.js
@@ -4,8 +4,8 @@ const SerialPort = require('serialport');
 // 연결된 시리얼 포트 콘솔 출력
 SerialPort.list().then(ports => {
   ports.forEach((port) => {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
+    console.log(port.comName);      // 연결되어 있는 포트 이름
+    console.log(port.pnpId);        // Plug N Play 아이디 
+    console.log(port.manufacturer); // 제조 업체
   });
 });
